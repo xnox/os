3f65fd3edf07957b86cd0ae92365f056e042547b
Dimitri John Ledkov <dimitri.ledkov@chainguard.dev>
2024-10-10 15:54:59 +0100

    use git for diffs

--- xnox.sh
+++ xnox.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 set -x
-ls *.yaml | xargs --replace={} --max-procs=$(nproc) sh -c 'melange scan {} >scans/old/{}.scan 2>/dev/null'
+ls *.yaml | xargs --replace={} --max-procs=$(nproc) sh -c 'melange scan {} >scans/{}.scan 2>/dev/null'
